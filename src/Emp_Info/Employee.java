f2f0a5fc6eee69252c0ade7b11bc3fe6559ce8a0
Anirudh Sampath Kumar <sampathkumar.an@northeastern.edu>
2022-09-27 12:26:18 -0400

    initial commit

---
+++ src/Emp_Info/Employee.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Emp_Info;
+//import java.util.Date;
+
+
+/**
+ *
+ * @author NRNirmala
+ */
+public class Employee {
+    private String name;
+    private int empId;
+    private int age;
+    private char sex;
+    private String strtDate;
+    private String level;
+    private String teamInfo;
+    private String position;
+    private String email;
+    private String phNmbr;
+//    private String photo;
+
+//    public String getPhoto() {
+//        return photo;
+//    }
+//
+//    public void setPhoto(String photo) {
+//        this.photo = photo;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getStrtDate() {
+        return strtDate;
+    }
+
+    public void setStrtDate(String strtDate) {
+        this.strtDate = strtDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhNmbr() {
+        return phNmbr;
+    }
+
+    public void setPhNmbr(String phNmbr) {
+        this.phNmbr = phNmbr;
+    }
+//
+//    public String getPhoto() {
+//        return photo;
+//    }
+//
+//    public void setPhoto(String photo) {
+//        this.photo = photo;
+//    }
+//    
+    
+}
